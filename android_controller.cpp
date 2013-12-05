e7bfbaba47bb66e3473a9e0fae2064293d89459f
marothon <marothon@gmail.com>
2013-12-05 15:33:01 +0100

    blue water + time based waves

--- android_controller.cpp
+++ android_controller.cpp
@@ -5,6 +5,7 @@ enum { NORTH=0, SOUTH=1, WEST=2, EAST=3 };
 AndroidController::AndroidController() : Controller(){
   receiver = thread(&PortListener::receiveData, PortListener());
 //  receiver.join();
+  last_time = glfwGetTime();
 };
 
 AndroidController::~AndroidController(){
@@ -22,19 +23,19 @@ bool AndroidController::apply_input(SPHSimulation* s, float dt){
   //s -> createCircularWave(vec2(0.0, 0.0), 0.11, 40);
   int direction;
   //if(abs(rot.x) > 45.0f && glfwGetTime() - last_time > 5.0f){
-  if(abs(rot.x) > 35.0f){
+  if(abs(rot.x) > 15.0f && glfwGetTime() - last_time > 2.0f){
     direction = NORTH;
     if(rot.x < 0 ) direction = SOUTH;
-    s -> createLinearWave(direction, 0.0, 1.0, abs(rot.x)/360.0f * 1000.0f);
-    //last_time = glfwGetTime();
+    s -> createLinearWave(direction, 0.0, 1.0, abs(rot.x)/360.0f * 800.0f);
+    last_time = glfwGetTime();
     return true;
   }
   //if(abs(rot.y) > 45.0f && glfwGetTime() - last_time > 5.0f){
-  if(abs(rot.y) > 35.0f){
-    direction = WEST;
-    if(rot.y < 0 ) direction = EAST;
-    s -> createLinearWave(direction, 0.0, 1.0, abs(rot.y)/360.0f * 1000.0f);
-    //last_time = glfwGetTime();
+  if(abs(rot.y) > 15.0f && glfwGetTime() - last_time > 2.0f){
+    direction = EAST;
+    if(rot.y < 0 ) direction = WEST;
+    s -> createLinearWave(direction, 0.0, 1.0, abs(rot.y)/360.0f * 800.0f);
+    last_time = glfwGetTime();
     return true;
   }
  
